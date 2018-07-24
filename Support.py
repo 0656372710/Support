b1294a38ee0fa2d7ccadeb234a20e10aff64e37f
kpopallstar <41643876+kpopallstar@users.noreply.github.com>
2018-07-24 17:56:20 +0100

    Update Support.py

--- Support.py
+++ Support.py
@@ -13,16 +13,16 @@ async def on_ready():
 @bot.event
 async def on_message(message):
             msg = getattr(message.server, 'id', None)
-            emb2 = (discord.Embed(color=0xff65a6))
+            emb2 = (discord.Embed(color=0xFFFF00))
             emb2.set_author(name="Support",
                             icon_url="https://media.discordapp.net/attachments/466276309501476874/470739248920657951/The_hall_support.png?width=480&height=480")
             emb2.add_field(name="**Thanks for the message!**", value="The hall team will get back to you as soon as possible!")
-            emb = (discord.Embed(description='**' + format(message.author.name) + ' Have A Problem !**', color=0xff65a6))
+            emb = (discord.Embed(description='**' + format(message.author.name) + ' Have A Problem !**', color=0xFFFF00))
             emb.set_author(name="The Problem :",
                             icon_url="https://media.discordapp.net/attachments/466276309501476874/470739248920657951/The_hall_support.png?width=480&height=480")
             emb.set_thumbnail(url=message.author.avatar_url)
-            emb.add_field(name="**Question**", value=message.content)
-            emb.add_field(name="**User ID**", value=message.author.id)
+            emb.add_field(name="**Question**", value=message.content,inline=False)
+            emb.add_field(name="**User ID**", value=message.author.id,inline=False)
             if msg !='453672511092293634' :
                 await bot.send_message(discord.Object(id='471067480316575744'),embed=emb)
                 await bot.send_message(message.author,embed=emb2)
@@ -31,9 +31,9 @@ async def on_message(message):
 @bot.command(pass_context=True)
 async def send(ctx,user:discord.Member):
     s=ctx.message.content
-    s2=s.split(',')[1]
+    s2=s.split('²')[1]
     if ctx.message.channel.id == '471067480316575744':
-        emb3 = (discord.Embed(description='**' + format(ctx.message.author.name) + ' Reply !**', color=0xff65a6))
+        emb3 = (discord.Embed(description='**' + format(ctx.message.author.name) + ' Reply !**', color=0xFFFF00))
         emb3.set_author(name=ctx.message.author.name,
                         icon_url=ctx.message.author.avatar_url)
         emb3.add_field(name="**Solution**", value=s2)
